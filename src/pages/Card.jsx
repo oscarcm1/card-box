ae08c054f77c4f2a60b64809b923305b23bdb774
oscar <oscar.delfos369.com>
2023-11-11 07:55:17 -0600

    card con video

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import "./card.css"
+import A from "../assets/b.png"
 
+import Ok from "../assets/ok.mp4"
 function Card() {
     return (
         <div className='box'>
+
+
             <div className='perpective'>
                 <div className='card'> 
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <button>Ver más</button>
                 </div>
+                <div className='background'>
+                    <video src={Ok}  autoPlay loop muted> </video>
+                 </div>
             </div>
+
+            <div className='perpective'>
+                <div className='card'> 
+                </div>
+                <div className='background'>
+                    <video src={Ok}  autoPlay loop muted> </video>
+                 </div>
+            </div>
+
+           
+
+
+
+
         </div>
     )
 }
