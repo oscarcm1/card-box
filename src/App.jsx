aa00ea578afbda579d4d439b9c2e3a0df561b875
oscar <oscar2905@gmail.com>
2023-12-28 02:39:45 -0600

    version 1 menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "./App.css"
+import { Logo , Hamburguer} from "./Icons";
 
-const Icon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-  </svg>
-)
+
 
 function App() {
 
@@ -16,8 +13,8 @@ function App() {
     <header className="navbar">
 
       <div className="logo">
-        <h1>Logo</h1>
-        <button onClick={active}><Icon /></button>
+         <Logo/>
+        <button onClick={active}>< Hamburguer/></button>
       </div>
 
       <nav className="menu">
